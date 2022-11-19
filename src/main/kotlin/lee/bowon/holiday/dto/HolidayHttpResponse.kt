0bed8658ea36e76666045c6ed931771944160503
bowon lee <asdfg3678@gmail.com>
2022-11-19 16:08:15 +0900

    xml 반환값 송신 데이터타입 생성

--- src/main/kotlin/lee/bowon/holiday/dto/HolidayHttpResponse.kt
+++ src/main/kotlin/lee/bowon/holiday/dto/HolidayHttpResponse.kt
@@ -1,8 +1,27 @@
 package lee.bowon.holiday.dto
 
-data class HolidayHttpResponse(
-    val pageNo: Int,
-    val numOfRows: Int,
-    val solYear: Int,
-    val solMonth:Int
+
+data class HolidayResponse(
+    val header: HolidayResponseHeader,
+    val body: HolidayResponseBody
 )
+
+data class HolidayResponseHeader(
+    val resultCode: String,
+    val resultMsg: String
+)
+
+data class HolidayResponseBody(
+    val items: Array<HolidayResponseItem>,
+    val numOfRows: Int,
+    val pageNo: Int,
+    val totalCount: Int
+)
+
+data class HolidayResponseItem(
+    val dataKind: String,
+    val isHoliday: String,
+    val locdate: String,
+    val seq: Int
+)
+
---
+++ src/test/kotlin/lee/bowon/holiday/service/HolidayClientTests.kt
@@ -0,0 +1,30 @@
+package lee.bowon.holiday.service
+
+import lee.bowon.holiday.dto.HolidayHttpRequest
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+
+
+import org.springframework.boot.test.context.SpringBootTest
+
+
+
+@SpringBootTest
+class HolidayClientTests {
+
+
+    @Autowired
+    private lateinit var holidayClient: HolidayClient
+
+    @Test
+    fun requestTest() {
+        val request = HolidayHttpRequest(2022, 10)
+
+        val result = this.holidayClient.getHolidayData(request)
+
+        compareValues(result.body.toString(), "")
+//        LoggerFactory.getILoggerFactory().getLogger().debug(request.)
+//
+//        compareValues(result, "")
+    }
+}
