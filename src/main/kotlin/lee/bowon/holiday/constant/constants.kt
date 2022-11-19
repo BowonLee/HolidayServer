2002bc258e49320068dc34279bc09d6f1a836a3b
bowon lee <asdfg3678@gmail.com>
2022-11-19 18:17:02 +0900

    endpoint url 변경

--- src/main/kotlin/lee/bowon/holiday/constant/constants.kt
+++ src/main/kotlin/lee/bowon/holiday/constant/constants.kt
@@ -1,6 +1,6 @@
 package lee.bowon.holiday.constant
 
 
-val END_POINT_URL = "http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService"
+const val END_POINT_URL = "http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getRestDeInfo"
 
 val API_KEY = "kwynZdE3L1Cu%2BUlj%2Bu2s6ujI7WrHFqvuIAJNY3Jaimy48r5EZrMPywC6Nw%2BOqK6Yxn1KGQ9dXufXlccW%2FaqNlA%3D%3D"
