8c27cbd79e20d8a877009ee0614d9eb7ed4a5a81
bowon lee <asdfg3678@gmail.com>
2022-11-21 23:37:27 +0900

    호출 test 케이스 작성

--- src/test/kotlin/lee/bowon/holiday/service/HolidayClientTests.kt
+++ src/test/kotlin/lee/bowon/holiday/service/HolidayClientTests.kt
@@ -1,7 +1,10 @@
 package lee.bowon.holiday.service
 
 import lee.bowon.holiday.dto.HolidayHttpRequest
+import lee.bowon.holiday.dto.HolidayResponseItem
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
 import org.springframework.beans.factory.annotation.Autowired
 
 
@@ -17,14 +20,23 @@ class HolidayClientTests {
     private lateinit var holidayClient: HolidayClient
 
     @Test
-    fun requestTest() {
+    fun response_empty() {
+        val request = HolidayHttpRequest("2022", "11")
+
+        val result = this.holidayClient.getHolidayData(request)
+
+        assert(result.isEmpty())
+    }
+
+    @Test
+    fun response_ok() {
         val request = HolidayHttpRequest("2022", "10")
 
         val result = this.holidayClient.getHolidayData(request)
 
-        compareValues(result.toString(), "")
-//        LoggerFactory.getILoggerFactory().getLogger().debug(request.)
-//
-//        compareValues(result, "")
+        val actual = HolidayResponseItem(dateName = "개천절", dateKind = "01", isHoliday = "Y", locdate = "20221003", seq = 1 )
+
+        assertEquals(result[0],actual)
+        assertEquals(result.size , 3)
     }
 }
