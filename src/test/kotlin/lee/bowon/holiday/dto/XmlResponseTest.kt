3a3ef4787817734510373a9e444f8056a5529a70
bowon lee <asdfg3678@gmail.com>
2022-11-24 00:58:35 +0900

    데이터 파싱 테스트케이스 작성

---
+++ src/test/kotlin/lee/bowon/holiday/dto/XmlResponseTest.kt
@@ -0,0 +1,25 @@
+package lee.bowon.holiday.dto
+
+import lee.bowon.holiday.enum.DateKind
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+@SpringBootTest
+class XmlResponseTest {
+
+
+    @Test
+    fun parseHolidayTest() {
+
+
+        val temp = XmlApiResponseItem(dateName = "개천절", dateKind = "01", isHoliday = "Y", locdate = "20221003", seq = 1 )
+
+        Assertions.assertEquals(temp.toHoliday().isHoliday, true )
+        Assertions.assertEquals(temp.toHoliday().date,  LocalDate.parse("20221003", DateTimeFormatter.BASIC_ISO_DATE))
+        Assertions.assertEquals(temp.toHoliday().dateKind, DateKind.NATIONAL.kindName )
+        Assertions.assertEquals(temp.toHoliday().dateName, "개천절")
+    }
+}
